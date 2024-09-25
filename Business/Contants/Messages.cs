30a81d7ebeff4e7ad0fb331c20ae32d6c0141c14
arzualkann <arzualkann@outlook.com>
2024-09-26 02:24:34 +0300

    Result altyapısı eklendi

---
+++ Business/Contants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintanceTime = "Bakım zamanı";
+        public static string ProductListed="ürünler listelendi";
+    }
+}
